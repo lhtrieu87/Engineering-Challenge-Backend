e629871a84f973f7ae9a716f6d9ce30f63e79b11
Trieu <pair+trieu@fastacash.com>
2015-07-08 10:07:47 +0800

    Initial setup for sails

---
+++ api/controllers/FoodItemController.js
@@ -0,0 +1,20 @@
+/**
+ * FoodItemController
+ *
+ * @description :: Server-side logic for managing Fooditems
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+
+  /**
+   * `FoodItemController.search()`
+   */
+  search: function(req, res) {
+    return res.json({
+      todo: 'search() is not implemented yet!'
+    });
+  }
+};
